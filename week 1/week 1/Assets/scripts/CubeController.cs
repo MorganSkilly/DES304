43263ba115f4ca535244653dbc1c24805f88b1c4
Morgan Skillicorn <hello@morgan.games>
2020-09-25 15:01:59 +0100

    Week 1
    week 1 lab work/unity project

---
+++ week 1/week 1/Assets/scripts/CubeController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+    // Start is called before Start
+    void Awake()
+    {
+        Debug.Log("Awake!");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Update!");
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Space clicked!");
+            GetComponent<Rigidbody>().AddForce(transform.TransformDirection(Vector3.up) * 5000);
+        }
+    }
+}
