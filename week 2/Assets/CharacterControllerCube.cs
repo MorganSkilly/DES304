fb900a446906f0f062beb9958053abdac7532d5e
Morgan Skillicorn <hello@morgan.games>
2020-10-02 00:46:03 +0100

    week 2

---
+++ week 2/Assets/CharacterControllerCube.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControllerCube : MonoBehaviour
+{
+    CharacterController characterController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = new Vector3(Input.GetAxis("Horizontal") * 0.1f, 0, Input.GetAxis("Vertical") * 0.1f);
+
+        characterController.Move(direction);
+    }
+}
